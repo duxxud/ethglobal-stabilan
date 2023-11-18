ef8531d68d4bb5478ba480c2221f0fb02059e8d9
kitanovicd <kitanovic.danilo@outlook.com>
2023-11-18 16:55:47 +0100

    feat: new deploy script

--- contracts/src/script/Deploy.s.sol
+++ contracts/src/script/Deploy.s.sol
@@ -6,6 +6,8 @@ import "../PriceFeedAggregator.sol";
 import "../TokenFactory.sol";
 import "../StabilanCore.sol";
 import "../libraries/Constants.sol";
+import "../mock/MockERC20.sol";
+import "../mock/MockChainlinkOracle.sol";
 
 contract Deploy is Script {
     function getChainId() public view returns (uint256) {
@@ -29,18 +31,47 @@ contract Deploy is Script {
 
         vm.startBroadcast(deployerPrivateKey);
 
-        Constants.ExternalAddresses memory externalAddresses = Constants.sepoliaAddresses();
+        Constants.ExternalAddresses memory externalAddresses = Constants
+            .sepoliaAddresses();
 
         PriceFeedAggregator priceFeedAggregator = new PriceFeedAggregator(
             deployerAddress
         );
-        console.log("PriceFeedAggregator address: ", address(priceFeedAggregator));
+        console.log(
+            "PriceFeedAggregator address: ",
+            address(priceFeedAggregator)
+        );
+        MockERC20 usdc = new MockERC20("USDC", "USDC");
+        usdc.mint(deployerAddress, 100000 ether);
+        console.log("USDC address: ", address(usdc));
 
-        priceFeedAggregator.setPriceFeed(externalAddresses.WETH_ADDRESS, externalAddresses.WETH_PRICE_FEED);
-        priceFeedAggregator.setPriceFeed(externalAddresses.USDC_ADDRESS, externalAddresses.USDC_PRICE_FEED);
-        priceFeedAggregator.setPriceFeed(externalAddresses.USDT_ADDRESS, externalAddresses.USDT_PRICE_FEED);
-        priceFeedAggregator.setPriceFeed(externalAddresses.DAI_ADDRESS, externalAddresses.DAI_PRICE_FEED);
-        priceFeedAggregator.setPriceFeed(externalAddresses.GHO_ADDRESS, externalAddresses.GHO_PRICE_FEED);
+        MockChainlinkOracle usdcPriceFeed = new MockChainlinkOracle();
+        usdcPriceFeed.setPrice(1 ether);
+        console.log("USDC price feed address: ", address(usdcPriceFeed));
+
+        MockERC20 usdt = new MockERC20("USDT", "USDT");
+        usdt.mint(deployerAddress, 100000 ether);
+        console.log("USDT address: ", address(usdt));
+
+        MockChainlinkOracle usdtPriceFeed = new MockChainlinkOracle();
+        usdtPriceFeed.setPrice(1 ether);
+        console.log("USDT price feed address: ", address(usdtPriceFeed));
+
+        MockERC20 dai = new MockERC20("DAI", "DAI");
+        dai.mint(deployerAddress, 100000 ether);
+        console.log("DAI address: ", address(dai));
+
+        MockChainlinkOracle daiPriceFeed = new MockChainlinkOracle();
+        daiPriceFeed.setPrice(1 ether);
+        console.log("DAI price feed address: ", address(daiPriceFeed));
+
+        priceFeedAggregator.setPriceFeed(
+            externalAddresses.WETH_ADDRESS,
+            externalAddresses.WETH_PRICE_FEED
+        );
+        priceFeedAggregator.setPriceFeed(address(usdc), address(usdcPriceFeed));
+        priceFeedAggregator.setPriceFeed(address(usdt), address(usdtPriceFeed));
+        priceFeedAggregator.setPriceFeed(address(dai), address(daiPriceFeed));
         console.log("Price feeds setted");
 
         TokenFactory tokenFactory = new TokenFactory();
--- contracts/src/script/deploy.sh
+++ contracts/src/script/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-forge script ./src/script/Deploy.s.sol --rpc-url='https://rpc.tenderly.co/fork/a17874e0-2e8c-453b-a01d-82aa79514644' --broadcast --verify
+forge script ./src/script/Deploy.s.sol --rpc-url='https://rpc.tenderly.co/fork/a17874e0-2e8c-453b-a01d-82aa79514644'  --broadcast --verify
  
