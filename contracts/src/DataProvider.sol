8a0331348700fc9bb16eac08849cca5dc2438500
DuXXuD <duxxud@gmail.com>
2023-11-19 03:42:38 +0100

    add data provider functions

--- contracts/src/DataProvider.sol
+++ contracts/src/DataProvider.sol
@@ -62,4 +62,62 @@ contract DataProvider {
 
         return userTokens;
     }
+
+    function getUserOptionTokens(IStabilanCore core, address account) external view returns (UserToken[] memory) {
+        UserToken[] memory userTokensTemp = new UserToken[](100);
+        uint256 userTokenLen = 0;
+
+        IOptionToken[] memory optionTokens;
+        (optionTokens,) = core.allStabilanTokens();
+
+        for (uint256 i = 0; i < optionTokens.length; i++) {
+            uint256 balance = optionTokens[i].balanceOf(account);
+            if (balance > 0) {
+                userTokensTemp[userTokenLen++] = UserToken({
+                    tokenType: TokenType.OPTION,
+                    stabilanTokenAddress: address(optionTokens[i]),
+                    undelyingAssetAddress: address(optionTokens[i].underlying()),
+                    backedAsset: address(0),
+                    endEpoch: optionTokens[i].endEpoch(),
+                    balance: balance
+                });
+            }
+        }
+
+        UserToken[] memory userTokens = new UserToken[](userTokenLen);
+        for (uint256 i = 0; i < userTokenLen; i++) {
+            userTokens[i] = userTokensTemp[i];
+        }
+
+        return userTokens;
+    }
+
+    function getUserBackingTokens(IStabilanCore core, address account) external view returns (UserToken[] memory) {
+        UserToken[] memory userTokensTemp = new UserToken[](100);
+        uint256 userTokenLen = 0;
+
+        IBackingToken[] memory backingTokens;
+        (, backingTokens) = core.allStabilanTokens();
+
+        for (uint256 i = 0; i < backingTokens.length; i++) {
+            uint256 balance = backingTokens[i].balanceOf(account);
+            if (balance > 0) {
+                userTokensTemp[userTokenLen++] = UserToken({
+                    tokenType: TokenType.BACKING,
+                    stabilanTokenAddress: address(backingTokens[i]),
+                    undelyingAssetAddress: address(backingTokens[i].underlying()),
+                    backedAsset: backingTokens[i].backedAsset(),
+                    endEpoch: backingTokens[i].endEpoch(),
+                    balance: balance
+                });
+            }
+        }
+
+        UserToken[] memory userTokens = new UserToken[](userTokenLen);
+        for (uint256 i = 0; i < userTokenLen; i++) {
+            userTokens[i] = userTokensTemp[i];
+        }
+
+        return userTokens;
+    }
 }
